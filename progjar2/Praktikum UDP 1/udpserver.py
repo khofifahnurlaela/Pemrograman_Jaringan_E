e6267b65ff591dfb4f95f18fe3e8c2405b1c077b
khofifahnurlaela <khofifahnurlaela11@gmail.com>
2021-07-22 18:11:49 +0700

    Menambahkan praktikum UDP 1

---
+++ progjar2/Praktikum UDP 1/udpserver.py
@@ -0,0 +1,15 @@
+import socket
+
+
+SERVER_IP = '192.168.122.243'
+SERVER_PORT = 5005
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((SERVER_IP, SERVER_PORT))
+
+while True:
+    data, addr = sock.recvfrom(1024)
+    # buffer size 1024
+    print("diterima ", data)
+    print("dikirim oleh ", addr)
